b45d889df092af1a2e10aadacf43fd6c1cc015f3
iCHAIT <cgupta319@gmail.com>
2017-09-19 19:52:41 +0800

    processed 6 features for LR model

--- train.py
+++ train.py
@@ -24,9 +24,109 @@ dataset = pd.read_csv("train.csv")
 
 # X_Train = pd.read_csv("train.csv", usecols = training_features).to_dict(orient="records")
 training_data = pd.read_csv("train.csv", usecols = training_features)
-# print(training_data.head())
 
-dummy = training_data.head()
+# print(type(training_data))
+# quit()
+
+for index, row in training_data.iterrows():
+	print(row['age'])
+
+	# Process age - biinning use weight of evidence
+	# row['age'] = something
+
+	# Process job
+	job_dict = {'admin.':1,'self-employed':2,'technician':3, 'management':4,'services':5,'student':6,
+	            'unemployed':7,'housemaid':8,'blue-collar':9,'entrepreneur':10,'retired':11,'unknown':12}
+
+	if row['job'] in job_dict:
+		row['job'] = job_dict[row['job']]
+
+	# print(row['job'])
+
+	# Process marital status
+	marital_status = {'single': 1, 'divorced': 2, 'married': 3, 'unknown': 4}
+
+	if row['marital'] in marital_status:
+		row['marital'] = marital_status[row['marital']]
+
+	# print(row['marital'])
+
+
+	# Process Education
+
+	education_dict = {'high.school':1,'university.degree':2,'professional.course':3,'unknown':4,
+                      'basic.6y':5,'basic.4y':6,'basic.9y':7,'illiterate':8}
+
+	if row['education'] in education_dict:
+		row['education'] = education_dict[row['education']]              
+
+	# Process Default
+
+	default_dict = {'unknown': 2, 'no': 0, 'yes': 1}
+
+	if row['default'] in default_dict:
+		row['default'] = default_dict[row['default']]
+
+	# print(row['default'])
+
+
+	# Process housing
+
+	housing_dict = {'unknown': 2, 'no': 0, 'yes': 1}
+
+	if row['housing'] in housing_dict:
+		row['housing'] = housing_dict[row['housing']]
+
+	# print(row['housing'])
+
+	# Process Loan
+
+	housing_dict = {'unknown': 2, 'no': 0, 'yes': 1}
+
+	if row['loan'] in housing_dict:
+		row['loan'] = housing_dict[row['loan']]
+
+	# print(row['loan'])
+
+
+	# Process contact
+
+	contact_dict = {'cellular': 1, 'telephone': 2}
+
+	if row['contact'] in contact_dict:
+		row['contact'] = contact_dict[row['contact']]
+
+
+	print(row['contact'])
+
+
+	# Process Month
+
+	month_dict = {'mar':1,'apr':2, 'may':3, 'jun':4, 'jul':5, 'aug':6, 'sep':7, 'oct':8, 'nov':9, 'dec':10}
+
+	if row['month'] in month_dict:
+		row['month'] = month_dict[row['month']]	
+
+	print(row['month'])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	quit()
+
 print(dummy)
 
 
