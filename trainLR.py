c2ed01fbee5ea5e66f15d923285b97e4148a96a0
iCHAIT <cgupta319@gmail.com>
2017-09-22 16:17:49 +0800

    pep8ififed code

--- trainLR.py
+++ trainLR.py
@@ -4,9 +4,9 @@ from sklearn.linear_model import LogisticRegression
 
 # Training Data - train.csv
 
-training_features = ['age','job','marital','education','default','housing','loan','contact','month',
-	                                       'day_of_week','duration','campaign','pdays','previous','poutcome','emp.var.rate',
-	                                       'cons.price.idx','cons.conf.idx','euribor3m','nr.employed']
+training_features = ['age', 'job', 'marital', 'education', 'default', 'housing', 'loan', 'contact', 'month',
+                     'day_of_week', 'duration', 'campaign', 'pdays', 'previous', 'poutcome', 'emp.var.rate',
+                     'cons.price.idx', 'cons.conf.idx', 'euribor3m', 'nr.employed']
 
 
 target = ["y"]
@@ -19,143 +19,127 @@ dataset = pd.read_csv("train.csv")
 # 	                                       'cons.price.idx','cons.conf.idx','euribor3m','nr.employed']).to_dict(orient="records")
 
 
-
-
-
 # X_Train = pd.read_csv("train.csv", usecols = training_features).to_dict(orient="records")
-training_data = pd.read_csv("train.csv", usecols = training_features)
+training_data = pd.read_csv("train.csv", usecols=training_features)
 
 # print(type(training_data))
 # quit()
 
 for index, row in training_data.iterrows():
-	print(row['age'])
+    print(row['age'])
 
-	# Process age - biinning use weight of evidence
-	# row['age'] = something
+    # Process age - biinning use weight of evidence
+    # row['age'] = something
 
-	# Process job
-	job_dict = {'admin.':1,'self-employed':2,'technician':3, 'management':4,'services':5,'student':6,
-	            'unemployed':7,'housemaid':8,'blue-collar':9,'entrepreneur':10,'retired':11,'unknown':12}
+    # Process job
+    job_dict = {'admin.': 1, 'self-employed': 2, 'technician': 3, 'management': 4, 'services': 5, 'student': 6,
+                'unemployed': 7, 'housemaid': 8, 'blue-collar': 9, 'entrepreneur': 10, 'retired': 11, 'unknown': 12}
 
-	if row['job'] in job_dict:
-		row['job'] = job_dict[row['job']]
+    if row['job'] in job_dict:
+        row['job'] = job_dict[row['job']]
 
-	# print(row['job'])
+    # print(row['job'])
 
-	# Process marital status
-	marital_status = {'single': 1, 'divorced': 2, 'married': 3, 'unknown': 4}
+    # Process marital status
+    marital_status = {'single': 1, 'divorced': 2, 'married': 3, 'unknown': 4}
 
-	if row['marital'] in marital_status:
-		row['marital'] = marital_status[row['marital']]
+    if row['marital'] in marital_status:
+        row['marital'] = marital_status[row['marital']]
 
-	# print(row['marital'])
+    # print(row['marital'])
 
+    # Process Education
 
-	# Process Education
+    education_dict = {'high.school': 1, 'university.degree': 2, 'professional.course': 3, 'unknown': 4,
+                      'basic.6y': 5, 'basic.4y': 6, 'basic.9y': 7, 'illiterate': 8}
 
-	education_dict = {'high.school':1,'university.degree':2,'professional.course':3,'unknown':4,
-                      'basic.6y':5,'basic.4y':6,'basic.9y':7,'illiterate':8}
+    if row['education'] in education_dict:
+        row['education'] = education_dict[row['education']]
 
-	if row['education'] in education_dict:
-		row['education'] = education_dict[row['education']]              
+    # Process Default
 
-	# Process Default
+    default_dict = {'unknown': 2, 'no': 0, 'yes': 1}
 
-	default_dict = {'unknown': 2, 'no': 0, 'yes': 1}
+    if row['default'] in default_dict:
+        row['default'] = default_dict[row['default']]
 
-	if row['default'] in default_dict:
-		row['default'] = default_dict[row['default']]
+    # print(row['default'])
 
-	# print(row['default'])
+    # Process housing
 
+    housing_dict = {'unknown': 2, 'no': 0, 'yes': 1}
 
-	# Process housing
+    if row['housing'] in housing_dict:
+        row['housing'] = housing_dict[row['housing']]
 
-	housing_dict = {'unknown': 2, 'no': 0, 'yes': 1}
+    # print(row['housing'])
 
-	if row['housing'] in housing_dict:
-		row['housing'] = housing_dict[row['housing']]
+    # Process Loan
 
-	# print(row['housing'])
+    housing_dict = {'unknown': 2, 'no': 0, 'yes': 1}
 
-	# Process Loan
+    if row['loan'] in housing_dict:
+        row['loan'] = housing_dict[row['loan']]
 
-	housing_dict = {'unknown': 2, 'no': 0, 'yes': 1}
+    # print(row['loan'])
 
-	if row['loan'] in housing_dict:
-		row['loan'] = housing_dict[row['loan']]
+    # Process contact
 
-	# print(row['loan'])
+    contact_dict = {'cellular': 1, 'telephone': 2}
 
+    if row['contact'] in contact_dict:
+        row['contact'] = contact_dict[row['contact']]
 
-	# Process contact
+    # print(row['contact'])
 
-	contact_dict = {'cellular': 1, 'telephone': 2}
+    # Process Month
 
-	if row['contact'] in contact_dict:
-		row['contact'] = contact_dict[row['contact']]
+    month_dict = {'mar': 1, 'apr': 2, 'may': 3, 'jun': 4,
+                  'jul': 5, 'aug': 6, 'sep': 7, 'oct': 8, 'nov': 9, 'dec': 10}
 
+    if row['month'] in month_dict:
+        row['month'] = month_dict[row['month']]
 
-	# print(row['contact'])
+    # print(row['month'])
 
+    # Process Weekday
 
-	# Process Month
+    week_dict = {'mon': 1, 'tue': 2, 'wed': 3, 'thu': 4, 'fri': 5}
 
-	month_dict = {'mar':1,'apr':2, 'may':3, 'jun':4, 'jul':5, 'aug':6, 'sep':7, 'oct':8, 'nov':9, 'dec':10}
+    if row['day_of_week'] in week_dict:
+        row['day_of_week'] = week_dict[row['day_of_week']]
 
-	if row['month'] in month_dict:
-		row['month'] = month_dict[row['month']]	
+    # print(row['day_of_week'])
 
-	# print(row['month'])
+    # Process Duration
 
+    duration = 100
 
-	# Process Weekday
+    if row['duration'] < duration:
+        row['duration'] = 0
+    else:
+        row['duration'] = 1
 
-	week_dict = {'mon': 1, 'tue': 2, 'wed': 3, 'thu': 4, 'fri': 5}
+    # print(row['duration'])
 
-	if row['day_of_week'] in week_dict:
-		row['day_of_week'] = week_dict[row['day_of_week']]	
+    # Process Campaign
 
+    camp = 25
 
-	# print(row['day_of_week'])
+    if row['campaign'] < camp:
+        row['campaign'] = 0
+    else:
+        row['campaign'] = 1
 
+    print(row['campaign'])
 
-	# Process Duration
-
-	duration = 100
-
-	if row['duration'] < duration:
-		row['duration'] = 0
-	else:
-		row['duration'] = 1
-
-	# print(row['duration'])
-
-
-	# Process Campaign
-
-	camp = 25
-
-	if row['campaign'] < camp:
-		row['campaign'] = 0
-	else:
-		row['campaign'] = 1
-
-	print(row['campaign'])
-
-
-
-
-
-
-	quit()
+    quit()
 
 print(dummy)
 
 
 # Y_Train = pd.read_csv("train.csv", usecols = target).to_dict(orient="records")
-Y_Train = pd.read_csv("train.csv", usecols = target)
+Y_Train = pd.read_csv("train.csv", usecols=target)
 
 # Now data is a list of all client records
 # print(X_Train)
@@ -165,8 +149,6 @@ Y_Train = pd.read_csv("train.csv", usecols = target)
 # for record in X_Train:
 # 	print(record)
 # 	quit()
-
-
 
 
 clf = LogisticRegression()
